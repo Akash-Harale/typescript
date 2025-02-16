51eed23a89ea73b9a88c43f0e100ffb5db5eb269
Akash <akash.v.harale@gmail.com>
2025-02-16 09:54:58 +0530

    finished : lecture no. 14

---
+++ section-2/14-object_types.ts
@@ -0,0 +1,16 @@
+let user: {
+  name: string;
+  age: number;
+  isAdmin: boolean;
+  id: string | number;
+};
+
+user = {
+  name: "Akash",
+  age: 22,
+  isAdmin: false,
+  id: 45,
+};
+
+// user={} // this will give error
+
